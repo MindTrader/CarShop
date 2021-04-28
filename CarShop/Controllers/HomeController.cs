4642e87d7b90e1112f50ab44e9afb8c8e6423859
Ilya Yurkevich <iliya.yurkevic@mail.ru>
2021-04-28 16:50:32 +0300

    HomePage

---
+++ CarShop/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarShop.Data.Interfaces;
+using CarShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IAllCars _carRep;
+
+        public HomeController(IAllCars carRep)
+        {
+            _carRep = carRep;
+        }
+
+        public ViewResult Index()
+        {
+            HomeViewModel homeCars = new HomeViewModel
+            {
+                FavCars = _carRep.GetFavCars
+            };
+            return View(homeCars);
+        }
+    }
+}
